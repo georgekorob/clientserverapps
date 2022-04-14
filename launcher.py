ef840d149ecabbad846e7614a6bff2377f53953d
George <georgekorob@gmail.com>
2022-04-14 16:41:32 +0300

    add launcher

---
+++ launcher.py
@@ -0,0 +1,30 @@
+# Лаунчер
+
+import subprocess
+import os
+
+env = os.environ.copy()
+env['PATH'] = '../venvs/clientservervenv/Scripts'
+address = '127.0.0.1'
+port = '8888'
+
+PROCESS = []
+
+while True:
+    ACTION = input('Выберите действие: q - выход, '
+                   's - запустить сервер и клиенты, x - закрыть все окна: ')
+
+    if ACTION == 'q':
+        break
+    elif ACTION == 's':
+        PROCESS.append(subprocess.Popen(['python', 'server.py', '-a', address, '-p', port],
+                                        creationflags=subprocess.CREATE_NEW_CONSOLE,
+                                        env=env))
+        for i in range(5):
+            PROCESS.append(subprocess.Popen(['python', 'client.py', address, port],
+                                            creationflags=subprocess.CREATE_NEW_CONSOLE,
+                                            env=env))
+    elif ACTION == 'x':
+        while PROCESS:
+            VICTIM = PROCESS.pop()
+            VICTIM.kill()
