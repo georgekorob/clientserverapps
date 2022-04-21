fdbb5b66ddf7150f82b40afd9b4f26de588f64d2
George <georgekorob@gmail.com>
2022-04-21 10:59:29 +0300

    launcher

--- launcher.py
+++ launcher.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import os
+import time
 
 env = os.environ.copy()
 env['PATH'] = '../venvs/clientservervenv/Scripts'
@@ -10,21 +11,22 @@ port = '8888'
 
 PROCESS = []
 
-while True:
-    ACTION = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, x - закрыть все окна: ')
-
-    if ACTION == 'q':
-        break
-    elif ACTION == 's':
-        PROCESS.append(subprocess.Popen(['python', 'server.py', '-a', address, '-p', port],
-                                        creationflags=subprocess.CREATE_NEW_CONSOLE,
-                                        env=env))
-        for i in range(5):
-            PROCESS.append(subprocess.Popen(['python', 'client.py', address, port],
-                                            creationflags=subprocess.CREATE_NEW_CONSOLE,
-                                            env=env))
-    elif ACTION == 'x':
-        while PROCESS:
-            VICTIM = PROCESS.pop()
-            VICTIM.kill()
+# while True:
+#     ACTION = input('Выберите действие: q - выход, '
+#                    's - запустить сервер и клиенты, x - закрыть все окна: ')
+#
+#     if ACTION == 'q':
+#         break
+#     elif ACTION == 's':
+PROCESS.append(subprocess.Popen(['python', 'server.py', '-a', address, '-p', port],
+                                creationflags=subprocess.CREATE_NEW_CONSOLE,
+                                env=env))
+for i in range(3):
+    PROCESS.append(subprocess.Popen(['python', 'client.py', address, port],
+                                    creationflags=subprocess.CREATE_NEW_CONSOLE,
+                                    env=env))
+#         elif ACTION == 'x':
+time.sleep(10)
+while PROCESS:
+    VICTIM = PROCESS.pop()
+    VICTIM.kill()
