c3af3e979ab07ff31dd955e9871750f783d6125d
George <georgekorob@gmail.com>
2022-04-21 10:48:08 +0300

    server

--- common/variables.py
+++ common/variables.py
@@ -1,4 +1,6 @@
 # Константы
+import logging
+
 
 # Порт по умолчанию для сетевого ваимодействия
 DEFAULT_PORT = 7777
@@ -10,6 +12,9 @@ MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_FILE_LEVEL = logging.DEBUG
+LOGGING_STREAM_LEVEL = logging.ERROR
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
