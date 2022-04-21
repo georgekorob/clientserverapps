90a3e0b793c44aa74805adadcfaea7961a9f16ac
George <georgekorob@gmail.com>
2022-04-21 10:59:15 +0300

    client

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 # Клиент
-
+import argparse
 import sys
 import json
 import socket
@@ -7,11 +7,27 @@ import time
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, DEFAULT_IP_ADDRESS,\
     DEFAULT_PORT
 from common.utils import get_message, send_message
+import logging
+import log.client_log_config
+
+CLIENT_LOGGER = logging.getLogger('client_logger')
 
 
 class Client:
-    def __init__(self, address=DEFAULT_IP_ADDRESS, port=DEFAULT_PORT):
+    def __init__(self, arguments):
         # Инициализация сокета
+        CLIENT_LOGGER.debug(f'Настройка клиента.')
+        CLIENT_LOGGER.debug(f'{arguments}')
+        parser = self.create_arg_parser()
+        namespace = parser.parse_args(arguments)
+        address, port = namespace.addr, namespace.port
+        # Валидация порта
+        if 1024 > port > 65535:
+            CLIENT_LOGGER.critical(f'Указан неподходящий порт: {port}. '
+                                   f'Допустимые адреса с 1024 до 65535.')
+            sys.exit(1)
+        CLIENT_LOGGER.info(f'Настроен клиент с парамертами: адрес {address}, порт {port}.')
+
         self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address, self.port = address, port
 
@@ -22,6 +38,7 @@ class Client:
         :param account_name: Имя пользователя.
         :return: Словарь запрос.
         """
+        CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
         return {
             ACTION: PRESENCE,
             TIME: time.time(),
@@ -37,6 +54,7 @@ class Client:
         :param message: Словарь-сообщение от сервера.
         :return: Информационная строка.
         """
+        CLIENT_LOGGER.debug(f'Сообщение от сервера: {message}')
         if RESPONSE in message:
             if message[RESPONSE] == 200:
                 return '200 : OK'
@@ -50,24 +68,19 @@ class Client:
         send_message(self.transport, message_to_server)
         try:
             answer = self.process_ans(get_message(self.transport))
-            print(answer)
+            CLIENT_LOGGER.info(f'Принят ответ от сервера: {answer}')
         except (ValueError, json.JSONDecodeError):
-            print('Не удалось декодировать сообщение сервера.')
+            CLIENT_LOGGER.error('Не удалось декодировать сообщение сервера.')
+
+    @staticmethod
+    def create_arg_parser():
+        """Парсер аргументов коммандной строки"""
+        parser = argparse.ArgumentParser()
+        parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+        parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+        return parser
 
 
 if __name__ == '__main__':
-    # Валидация адреса и порта
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-
-    client = Client(server_address, server_port)
+    client = Client(sys.argv[1:])
     client.start()
