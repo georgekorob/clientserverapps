7f382c54b269bd794ecd88db2254a7abf604fce5
George <georgekorob@gmail.com>
2022-04-25 09:10:58 +0300

    server

--- log/server_log_config.py
+++ log/server_log_config.py
@@ -16,11 +16,12 @@ STREAM_HANDLER.setLevel(LOGGING_STREAM_LEVEL)
 
 FILE_HANDLER = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='D')
 FILE_HANDLER.setFormatter(FORMATTER)
+FILE_HANDLER.setLevel(LOGGING_FILE_LEVEL)
 
 LOGGER = logging.getLogger('server_logger')
 LOGGER.addHandler(STREAM_HANDLER)
 LOGGER.addHandler(FILE_HANDLER)
-LOGGER.setLevel(LOGGING_FILE_LEVEL)
+LOGGER.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     LOGGER.critical('Критическая ошибка')
