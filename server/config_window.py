3ab977c85225a7383ed5abc454e1226aa923a77c
George <georgekorob@gmail.com>
2022-06-03 23:14:45 +0300

    windows

--- server/config_window.py
+++ server/config_window.py
@@ -1,16 +1,21 @@
-from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
+from PyQt5.QtCore import Qt
+import os
 
 
 class ConfigWindow(QDialog):
     """Окно настроек."""
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
         self.initUI()
 
     def initUI(self):
-        # Настройки окна
+        '''Настройки окна'''
         self.setFixedSize(365, 260)
         self.setWindowTitle('Настройки сервера')
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setModal(True)
 
         # Надпись о файле базы данных:
         self.db_path_label = QLabel('Путь до файла базы данных: ', self)
@@ -26,16 +31,6 @@ class ConfigWindow(QDialog):
         # Кнопка выбора пути.
         self.db_path_select = QPushButton('Обзор...', self)
         self.db_path_select.move(275, 28)
-
-        # Функция обработчик открытия окна выбора папки
-        def open_file_dialog():
-            global dialog
-            dialog = QFileDialog(self)
-            path = dialog.getExistingDirectory()
-            path = path.replace('/', '\\')
-            self.db_path.insert(path)
-
-        self.db_path_select.clicked.connect(open_file_dialog)
 
         # Метка с именем поля файла базы данных
         self.db_file_label = QLabel('Имя файла базы данных: ', self)
@@ -81,4 +76,43 @@ class ConfigWindow(QDialog):
         self.close_button.move(275, 220)
         self.close_button.clicked.connect(self.close)
 
+        self.db_path_select.clicked.connect(self.open_file_dialog)
+
         self.show()
+
+        self.db_path.insert(self.config['SETTINGS']['Database_path'])
+        self.db_file.insert(self.config['SETTINGS']['Database_file'])
+        self.port.insert(self.config['SETTINGS']['Default_port'])
+        self.ip.insert(self.config['SETTINGS']['Listen_Address'])
+        self.save_btn.clicked.connect(self.save_server_config)
+
+    def open_file_dialog(self):
+        '''Метод обработчик открытия окна выбора папки.'''
+        global dialog
+        dialog = QFileDialog(self)
+        path = dialog.getExistingDirectory()
+        path = path.replace('/', '\\')
+        self.db_path.clear()
+        self.db_path.insert(path)
+
+    def save_server_config(self):
+        '''Сохранение настроек.'''
+        global config_window
+        message = QMessageBox()
+        self.config['SETTINGS']['Database_path'] = self.db_path.text()
+        self.config['SETTINGS']['Database_file'] = self.db_file.text()
+        try:
+            port = int(self.port.text())
+        except ValueError:
+            message.warning(self, 'Ошибка', 'Порт должен быть числом')
+        else:
+            self.config['SETTINGS']['Listen_Address'] = self.ip.text()
+            if 1023 < port < 65536:
+                self.config['SETTINGS']['Default_port'] = str(port)
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                dir_path = os.path.join(dir_path, '..')
+                with open(f"{dir_path}/{'server.ini'}", 'w') as conf:
+                    self.config.write(conf)
+                    message.information(self, 'OK', 'Настройки успешно сохранены!')
+            else:
+                message.warning(self, 'Ошибка', 'Порт должен быть от 1024 до 65536')
